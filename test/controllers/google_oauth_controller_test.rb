ec6ecec671a2732ced96b013cc00cb80a010d718
OkemotoHiroki <okemoto@antas.jp>
2026-04-14 18:27:43 +0900

    fix:googleoauthcontroller test

--- test/controllers/google_oauth_controller_test.rb
+++ test/controllers/google_oauth_controller_test.rb
@@ -3,11 +3,17 @@ require "test_helper"
 class GoogleOauthControllerTest < ActionDispatch::IntegrationTest
   test "should get start" do
     get google_oauth_start_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get callback" do
-    get google_oauth_callback_url
-    assert_response :success
+    fake_token = Struct.new(:token, :refresh_token).new("token123", "refresh123")
+
+    GoogleOauthService.stub(:exchange, fake_token) do
+      get google_oauth_callback_url, params: { code: "test_code" }
+
+      assert_response :redirect
+      assert_equal "token123", session[:access_token]
+    end
   end
 end
